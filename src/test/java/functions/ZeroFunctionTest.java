75d7e057151d5ce9afcaf84174c3cb467bbd9591
Migalin <evseviusfromkesaria@gmail.com>
2023-09-29 22:22:07 +0400

    Поехали

---
+++ src/test/java/functions/ZeroFunctionTest.java
@@ -0,0 +1,19 @@
+package functions;
+
+import junit.framework.TestCase;
+
+public class ZeroFunctionTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testApply() {
+    }
+
+    public void testAndThen() {
+    }
+}
