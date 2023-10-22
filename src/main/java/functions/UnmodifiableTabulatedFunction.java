9d675bff85575a0bec227a8fee2f6daad9475f33
Migalin <evseviusfromkesaria@gmail.com>
2023-10-22 14:06:39 +0400

    Добавил UnmodifiableTabulatedFunction

---
+++ src/main/java/functions/UnmodifiableTabulatedFunction.java
@@ -0,0 +1,63 @@
+package functions;
+
+import java.util.Iterator;
+
+public class UnmodifiableTabulatedFunction implements TabulatedFunction {
+
+    TabulatedFunction function;
+
+    public UnmodifiableTabulatedFunction(TabulatedFunction func) {
+        function = func;
+    }
+
+    @Override
+    public int getCount() {
+        return function.getCount();
+    }
+
+    @Override
+    public double getX(int index) {
+        return function.getX(index);
+    }
+
+    @Override
+    public double getY(int index) {
+        return function.getY(index);
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public double leftBound() {
+        return function.leftBound();
+    }
+
+    @Override
+    public double rightBound() {
+        return function.rightBound();
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        return function.indexOfX(x);
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        return function.indexOfY(y);
+    }
+
+    @Override
+    public double apply(double x) {
+        return function.apply(x);
+    }
+
+    @Override
+    public Iterator<Point> iterator() {
+        return function.iterator();
+    }
+}
+
