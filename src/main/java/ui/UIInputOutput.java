1456c1b1390e7da92dc4d0950e350b06dae73ab2
Migalin <evseviusfromkesaria@gmail.com>
2023-12-01 22:23:32 +0400

    XML и JSON

--- src/main/java/ui/UIInputOutput.java
+++ src/main/java/ui/UIInputOutput.java
@@ -2,6 +2,7 @@ package ui;
 
 import functions.ArrayTabulatedFunction;
 import functions.TabulatedFunction;
+import io.ArrayTabulatedFunctionXMLSerialization;
 import io.FunctionsIO;
 import javafx.scene.control.Tab;
 import javafx.stage.FileChooser;
@@ -13,12 +14,11 @@ public class UIInputOutput {
     static public void saveBin(TabulatedFunction function) throws IOException {
 
         FileChooser fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Bin files", "*.bin"), new FileChooser.ExtensionFilter("JSON files", "*.json"));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Bin files", "*.bin"), new FileChooser.ExtensionFilter("JSON files", "*.json"), new FileChooser.ExtensionFilter("XML files", "*.xml"));
 
         File file = fileChooser.showSaveDialog(null);
 
         String extension = file.getName().substring(file.getName().lastIndexOf(".")+1);
-        System.out.println(extension);
 
         switch (extension){
             case "bin":
@@ -29,43 +29,44 @@ public class UIInputOutput {
             case "json":
                 BufferedWriter writer = new BufferedWriter( new FileWriter(file));
                 FunctionsIO.serializeJson(writer, (ArrayTabulatedFunction) function);
-
-        }
-
-        FileOutputStream outputStream = new FileOutputStream(file);
-        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
-
-        FunctionsIO.serialize(bufferedOutputStream, function);
+                break;
+            case "xml":
+                BufferedWriter writer1 = new BufferedWriter( new FileWriter(file));
+                FunctionsIO.serializeXml(writer1, function);
+                break;
+        };
 
     }
 
     static public TabulatedFunction loadBin() throws IOException, ClassNotFoundException {
 
         FileChooser fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Bin files", "*.bin"), new FileChooser.ExtensionFilter("JSON files", "*.json"));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Bin files", "*.bin"), new FileChooser.ExtensionFilter("JSON files", "*.json"), new FileChooser.ExtensionFilter("XML files", "*.xml"));
 
         File file = fileChooser.showOpenDialog(null);
 
         String extension = file.getName().substring(file.getName().lastIndexOf(".")+1);
 
         TabulatedFunction function = null;
+
         switch (extension){
             case "bin":
                 FileInputStream inputStream = new FileInputStream(file);
                 BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
                 function = FunctionsIO.deserialize(bufferedInputStream);
                 break;
-            case "xml":
+            case "json":
                 BufferedReader reader = new BufferedReader( new FileReader(file));
-                function = FunctionsIO.deserializeXml(reader);
+                function = FunctionsIO.deserializeJson(reader);
+                break;
+            case "xml":
+                BufferedReader reader1 = new BufferedReader( new FileReader(file));
+                function = FunctionsIO.deserializeXml(reader1);
+                break;
 
         }
 
         return function;
-
-
-
-
 
     }
 
